083b952782114c44b358c02f5fa0402e1abcd786
RedPortal <msca8h@naver.com>
2017-01-04 23:46:09 +0900

    implementing MVC pattern to unfold this pile of spaghetti

---
+++ Lighthouse for Visual Studio/LighthouseClassBrowser/LighthouseModels.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LighthouseClassBrowser
+{
+    internal class ProjectBrowserModel
+    {
+        
+    }
+
+    internal class ClassBrowserModel
+    {
+        
+    }
+
+    internal class MethodBrowserModel
+    {
+        
+    }
+
+    internal class VariableBrowserModel
+    {
+        
+    }
+}
